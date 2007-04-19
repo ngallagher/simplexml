97b663426025333e9cdcc24a440381aaaf786196
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2007-04-19 20:11:18 +0000

    Update documentation for the array type

--- src/simple/xml/load/ArrayType.java
+++ src/simple/xml/load/ArrayType.java
@@ -23,20 +23,61 @@ package simple.xml.load;
 import java.lang.reflect.Array;
 import java.util.List;
 
+/**
+ * The <code>ArrayType</code> object is a type used for constructing
+ * arrays from a specified component type or <code>Type</code> object.
+ * This allows primitive and composite lists to be acquired either by
+ * reference or by value from a converter object. This will accept a
+ * list of objects which it will convert to an array object.
+ * 
+ * @author Niall Gallagher
+ * 
+ * @see simple.xml.load.ArrayFactory
+ */
 final class ArrayType implements Type {
    
+   /**
+    * This is the optional field type for the array to be created. 
+    */
    private Class field;
    
+   /**
+    * This is used if the array is a reference to an instance.
+    */
    private Type type;
    
+   /**
+    * This is used to specify the creation of an array type which
+    * can be used for creating an array that can hold instances of 
+    * the specified type. The type specified must be the component
+    * type for the array that is to be created. 
+    * 
+    * @param field this is the component type for the array
+    */
    public ArrayType(Class field) {
       this.field = field;      
    }
    
+   /**
+    * This is used to construct an array from a reference to an
+    * existing array. The type specified must be a reference. If it
+    * is not a reference to an existing array type type returned is
+    * dependant on the implementation and could be a null value. 
+   
+    * @param type this is the type that references an array
+    */
    public ArrayType(Type type) {
       this.type = type;
    }
    
+   /**
+    * This is the instance that is acquired from this type. This is
+    * typically used if the <code>isReference</code> method is true.
+    * If there was to type reference provided then this returns null
+    * otherwise this will delegate to the <code>Type</code> given.
+    * 
+    *  @return this returns a reference to an existing array
+    */
    public Object getInstance() throws Exception {
       if(type != null) {
         return type.getInstance();
@@ -44,6 +85,17 @@ final class ArrayType implements Type {
       return null;
    }
    
+   /**
+    * Creates the object array to use. This will use the provided
+    * list of values to form the values within the array. Each of
+    * the values witin the specified <code>List</code> will be
+    * set into a the array, if the type of the values within the
+    * list are not compatible then an exception is thrown.
+    * 
+    * @param list this is the list of values for the array    
+    * 
+    * @return this is the obejct array instantiated for the type
+    */
    public Object getInstance(List list) throws Exception {
       return getInstance(list, list.size());
    }
@@ -69,14 +121,29 @@ final class ArrayType implements Type {
       return array;     
    }
    
+   /**
+    * This will return the component type for the array instance 
+    * that is produced by this object. Depending on the constructor 
+    * used this will either delegate to the <code>Type</code> object 
+    * specified or will return the component type class provided. 
+    * 
+    * @return this returns the component type for the array
+    */
    public Class getType() {
       if(type != null) {
          return type.getType();
       }
-      return null;
+      return field;
    }
    
-   
+   /**
+    * This will return true if the <code>Type</code> object provided
+    * is a reference type. Typically a reference type refers to a 
+    * type that is substituted during the deserialization process 
+    * and so constitutes an object that does not need initialization.
+    * 
+    * @retunr this returns true if the type is a reference type
+    */
    public boolean isReference() {
       if(type != null) {
          return type.isReference();
