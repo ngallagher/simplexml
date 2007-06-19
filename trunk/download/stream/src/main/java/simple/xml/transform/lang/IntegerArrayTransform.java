c60aff52b8f00345464c250e2a629a39ccab12d1
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2007-06-19 22:54:36 +0000

    Add the integer array transform

---
+++ trunk/download/stream/src/main/java/simple/xml/transform/lang/IntegerArrayTransform.java
@@ -0,0 +1,44 @@
+
+
+package simple.xml.transform;
+
+import simple.xml.transform.Transform;
+
+public class IntegerArrayTransform implements Transform<Integer[]> {
+
+   private final StringArrayTransform split;
+
+   private final Transform single;
+
+   public IntegerArrayTransform() {
+      this.split = new StringArrayTransform();           
+      this.single = new IntegerTransform();
+   }        
+
+   public Integer[] read(String value) {
+      String[] list = split.read(value);
+
+      if(list.length > 0) {   
+         return read(list);
+      }
+      return new Integer[]{};
+   }
+
+   private Integer[] read(String[] text) {
+      Integer[] list = new Integer[text.length];
+
+      for(int i = 0; i < text.length; i++) {
+         list[i] = single.read(entry);
+      }
+      return list;
+   }
+
+   public String write(Integer[] list) {
+      String[] text = new String[list.length];           
+
+      for(int i = 0; i < text.length; i++) {
+         text[i] = single.read(list[i]);
+      }
+      return split.write(text);
+   }
+}
