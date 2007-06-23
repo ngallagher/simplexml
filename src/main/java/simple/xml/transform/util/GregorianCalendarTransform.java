b23c50fa99bfe40dca97aaa983024b2b928b3f3b
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2007-06-23 16:12:13 +0000

    Update date formatter

--- src/main/java/simple/xml/transform/util/GregorianCalendarTransform.java
+++ src/main/java/simple/xml/transform/util/GregorianCalendarTransform.java
@@ -3,14 +3,27 @@ package simple.xml.transform.util;
 
 import simple.xml.transform.Transform;
 import java.util.GregorianCalendar;
+import java.util.Date;
 
 public class GregorianCalendarTransform implements Transform<GregorianCalendar> {
    
+   private DateTransform transform;
+   
+   public GregorianCalendarTransform() {
+      this.transform = new DateTransform();
+   }
+   
    public GregorianCalendar read(String value) throws Exception {
-      return null;
+      return read(transform.read(value));      
+   }
+   
+   private GregorianCalendar read(Date value) throws Exception {
+      GregorianCalendar calendar = new GregorianCalendar();
+      calendar.setTime(value);
+      return calendar;
    }
    
    public String write(GregorianCalendar value) throws Exception {
-      return null;
+      return transform.write(value.getTime());
    }
 }
