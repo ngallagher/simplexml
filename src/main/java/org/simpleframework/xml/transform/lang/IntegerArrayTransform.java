497687ac464bd63779ebe266f676a4919d24971b
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2007-06-24 00:05:32 +0000

    Update documentation

--- src/main/java/org/simpleframework/xml/transform/lang/IntegerArrayTransform.java
+++ src/main/java/org/simpleframework/xml/transform/lang/IntegerArrayTransform.java
@@ -45,13 +45,24 @@ import org.simpleframework.xml.transform.Transform;
  */
 public class IntegerArrayTransform implements Transform<Integer[]> {
 
+   /**
+    * Used to create and populate the array using another transform. 
+    */
    private final ArrayTransform array;
    
+   /**
+    * Acts as the transform used to transform each array entry.
+    */
    private final Transform single;
    
+   /**
+    * Constructor for transform object. This creates a transform 
+    * for creating an array which can then be written and read to
+    * and from an XML document. This splits the array values using
+    * a comma separated list of string entry values.
+    */
    public IntegerArrayTransform() {
-      this.array = new ArrayTransform(Integer.class);   
-      
+      this.array = new ArrayTransform(Integer.class);       
       this.single = new IntegerTransform();
    }
    
