97d3b8bd78cefe79be2478e751f70337f5b47157
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2007-06-24 12:08:28 +0000

    Update documentation

--- src/main/java/org/simpleframework/xml/transform/DefaultMatcher.java
+++ src/main/java/org/simpleframework/xml/transform/DefaultMatcher.java
@@ -20,14 +20,43 @@
 
 package org.simpleframework.xml.transform;
 
+/**
+ * The <code>DefaultMatcher</code> object is used to match types to
+ * their transform types. This makes use of a convention in order to
+ * map the specified type to its transform. The convention used is
+ * to append "Transform" to the fully qualified name of the type to
+ * be transformed, or "ArrayType" if the type is an array.
+ * 
+ * @author Niall Gallagher
+ */
 class DefaultMatcher extends PackageMatcher {
    
+   /**
+    * This is a matcher used to resolve the transforms for arrays.
+    */
    private final Matcher array;
    
+   /**
+    * Constructor for the <code>DefaultMatcher</code> object. This 
+    * is used to create matcher instance that matches transforms for
+    * types using conventions where the fully qualified class name
+    * for the type to be matched is appended with a known suffix.
+    */
    public DefaultMatcher() {
       this.array = new ArrayMatcher(this);
    }
    
+   /**
+    * This is used to match a <code>Transform</code> using the type
+    * specified. If no transform can be acquired then an exception
+    * is thrown indicating that no transform could be found.
+    * 
+    * @param type this is the type to acquire the transform for
+    * 
+    * @return returns a transform for processing the type given
+    * 
+    * @throws Exception thrown if a transform could not be found
+    */    
    public Transform match(Class type) throws Exception {
       if(type.isArray()) {
          return array.match(type);
@@ -35,6 +64,17 @@ class DefaultMatcher extends PackageMatcher {
       return matchType(type);
    }
    
+   /**
+    * This is used to match a <code>Transform</code> using the type
+    * specified. If no transform can be acquired then an exception
+    * is thrown indicating that no transform could be found.
+    * 
+    * @param type this is the type to acquire the transform for
+    * 
+    * @return returns a transform for processing the type given
+    * 
+    * @throws Exception thrown if a transform could not be found
+    */   
    private Transform matchType(Class type) throws Exception {
       Class real = getConversion(type);
       Class match = getClass(real);
@@ -42,6 +82,16 @@ class DefaultMatcher extends PackageMatcher {
       return (Transform)match.newInstance();      
    }
    
+   /**
+    * Primitive types are transformed using their respective Java 
+    * language types. So in order to figure out the transform that
+    * is required for the type it needs to be converted so that it
+    * can be resolved using the name convention scheme. 
+    * 
+    * @param type this is the primitive type to be converted 
+    * 
+    * @return returns the Java language type for the primitive
+    */
    private Class getConversion(Class type) {
       if(type == int.class) {
          return Integer.class;              
--- src/main/java/org/simpleframework/xml/transform/Transformer.java
+++ src/main/java/org/simpleframework/xml/transform/Transformer.java
@@ -1,21 +1,103 @@
+/*
+ * Transformer.java May 2007
+ *
+ * Copyright (C) 2007, Niall Gallagher <niallg@users.sf.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General 
+ * Public License along with this library; if not, write to the 
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330, 
+ * Boston, MA  02111-1307  USA
+ */
 
 package org.simpleframework.xml.transform;
 
+/**
+ * The <code>Transformer</code> object is used to convert strings to
+ * and from object instances. This is used during the serialization
+ * and deserialization process to transform types from the Java class
+ * libraries, as well as other types which do not contain XML schema
+ * annotations. Typically this will be used to transform primitive
+ * types to and from strings, such as <code>int</code> values.
+ * <pre>
+ * 
+ *    &#64;Element
+ *    private String[] value;
+ *    
+ * <pre>
+ * For example taking the above value the array of strings needs to 
+ * be converted in to a single string value that can be inserted in 
+ * to the element in such a way that in can be read later. In this
+ * case the serialized value of the string array would be as follows.
+ * <pre>
+ * 
+ *    &lt;value&gt;one, two, three&lt;/value&gt;
+ * 
+ * </pre>
+ * Here each non-null string is inserted in to a comma separated  
+ * list of values, which can later be deserialized. Just to note the
+ * above array could be annotated with <code>ElementList</code> just
+ * as easily, in which case each entry would have its own element.
+ * The choice of which annotation to use is up to the developer. A
+ * more obvious benifit to transformations like this can be seen for
+ * values annotated with the <code>Attribute</code> annotation.
+ * 
+ * @author Niall Gallagher
+ */
 public class Transformer {
 
+   /**
+    * This is used to cache all transforms matched to a given type.
+    */
    private TransformCache cache;
    
+   /**
+    * This is used to perform the matching of types to transforms.
+    */
    private Matcher matcher;
    
+   /**
+    * Constructor for the <code>Transformer</code> object. This is
+    * used to create a transformer which will transform specified
+    * types using transforms loaded from the class path. By default
+    * this will match transforms by convention.
+    */
    public Transformer() {
       this(new DefaultMatcher());
    }
    
+   /**
+    * Constructor for the <code>Transformer</code> object. This is
+    * used to create a transformer which will transform specified
+    * types using transforms loaded from the class path. Tramsforms
+    * are matched to types using the specified matcher object.
+    * 
+    * @param matcher this is used to match types to transforms
+    */
    private Transformer(Matcher matcher) {      
       this.cache = new TransformCache();
       this.matcher = matcher;
    }
    
+   /**
+    * This method is used to convert the string value given to an
+    * appropriate representation. This is used when an object is
+    * being deserialized from the XML document and the value for
+    * the string representation is required.
+    * 
+    * @param value this is the string representation of the value
+    * @param type this is the type to convert the string value to
+    * 
+    * @return this returns an appropriate instanced to be used
+    */
    public Object read(String value, Class type) throws Exception {
       Transform transform = cache.get(type);
       
@@ -25,7 +107,18 @@ public class Transformer {
       }
       return transform.read(value);
    }
-
+   
+   /**
+    * This method is used to convert the provided value into an XML
+    * usable format. This is used in the serialization process when
+    * there is a need to convert a field value in to a string so 
+    * that that value can be written as a valid XML entity.
+    * 
+    * @param value this is the value to be converted to a string
+    * @param type this is the type to convert to a string value
+    * 
+    * @return this is the string representation of the given value
+    */
    public String write(Object value, Class type) throws Exception {
       Transform transform = cache.get(type);
       
