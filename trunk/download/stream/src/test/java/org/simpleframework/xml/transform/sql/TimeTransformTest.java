5abad7ff5fc0331db59187fa25f1ff23394a1c8c
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2007-06-24 14:34:48 +0000

    Update tests

--- trunk/download/stream/src/test/java/org/simpleframework/xml/transform/sql/TimeTransformTest.java
+++ trunk/download/stream/src/test/java/org/simpleframework/xml/transform/sql/TimeTransformTest.java
@@ -1,9 +1,33 @@
 package org.simpleframework.xml.transform.sql;
 
 import java.sql.Time;
+
 import junit.framework.TestCase;
 
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
 public class TimeTransformTest extends TestCase {
+   
+   @Root
+   public class TimeExample {
+      
+      @Attribute     
+      private Time attribute;
+      
+      @Element
+      private Time element;      
+      
+      public TimeExample() {
+         super();
+      }
+      
+      public TimeExample(long time) {
+         this.attribute = new Time(time);
+         this.element = new Time(time);
+      }   
+   }
    
    public void testTime() throws Exception {
       long now = System.currentTimeMillis();
