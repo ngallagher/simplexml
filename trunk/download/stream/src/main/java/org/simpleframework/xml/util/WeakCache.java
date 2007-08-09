7c9713062e63556417b1dab05a2eb481fb0d5c73
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2007-08-09 16:09:36 +0000

    Update class loading

--- trunk/download/stream/src/main/java/org/simpleframework/xml/util/WeakCache.java
+++ trunk/download/stream/src/main/java/org/simpleframework/xml/util/WeakCache.java
@@ -32,6 +32,8 @@ import java.util.List;
  * maps in a way that lowers contention for the locks used. 
  * 
  * @author Niall Gallagher
+ * 
+ * @see java.util.concurrent.ConcurrentHashMap
  */
 public class WeakCache<K, V> implements Cache<K, V> {
    
