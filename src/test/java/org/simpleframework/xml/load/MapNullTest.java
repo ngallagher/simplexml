f2cb740694071de9f8ea162156846543acb85002
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2008-03-04 20:18:02 +0000

    Add new tests

--- src/test/java/org/simpleframework/xml/load/MapNullTest.java
+++ src/test/java/org/simpleframework/xml/load/MapNullTest.java
@@ -13,189 +13,44 @@ import org.simpleframework.xml.ValidationTestCase;
 
 public class MapNullTest extends ValidationTestCase {
    
-   private static final String ENTRY_MAP =     
-   "<entryMap>\n"+
-   "   <map>\n"+   
-   "      <entry key='a'>" +
-   "         <mapEntry>\n" +  
-   "            <name>a</name>\n"+
-   "            <value>example 1</value>\n"+
-   "         </mapEntry>" + 
-   "      </entry>" +
-   "      <entry key='b'>" +
-   "         <mapEntry>\n" +  
-   "            <name>b</name>\n"+
-   "            <value>example 2</value>\n"+
-   "         </mapEntry>" + 
-   "      </entry>" +
-   "      <entry key='c'>" +
-   "         <mapEntry>\n" +  
-   "            <name>c</name>\n"+
-   "            <value>example 3</value>\n"+
-   "         </mapEntry>" + 
-   "      </entry>" +
-   "      <entry key='d'>" +
-   "         <mapEntry>\n" +  
-   "            <name>d</name>\n"+
-   "            <value>example 4</value>\n"+
-   "         </mapEntry>" + 
-   "      </entry>" +
-   "   </map>\n"+
-   "</entryMap>";  
    
-   private static final String STRING_MAP =     
-   "<stringMap>\n"+
-   "   <map>\n"+   
-   "      <entry letter='a'>example 1</entry>\n" +
-   "      <entry letter='b'>example 2</entry>\n" +
-   "      <entry letter='c'>example 3</entry>\n" +
-   "      <entry letter='d'>example 4</entry>\n" +   
-   "   </map>\n"+
-   "</stringMap>";
-   
-   private static final String COMPLEX_MAP =     
-   "<complexMap>\n"+
-   "   <map>\n"+   
-   "      <entry>" +
-   "         <compositeKey>\n" +
-   "            <name>name 1</name>\n" +
-   "            <address>address 1</address>\n" +
-   "         </compositeKey>\n" +
-   "         <mapEntry>\n" +  
-   "            <name>a</name>\n"+
-   "            <value>example 1</value>\n"+
-   "         </mapEntry>" + 
-   "      </entry>" +
-   "      <entry>" +
-   "         <compositeKey>\n" +
-   "            <name>name 2</name>\n" +
-   "            <address>address 2</address>\n" +
-   "         </compositeKey>\n" +
-   "         <mapEntry>\n" +  
-   "            <name>b</name>\n"+
-   "            <value>example 2</value>\n"+
-   "         </mapEntry>" + 
-   "      </entry>" +
-   "      <entry>" +
-   "         <compositeKey>\n" +
-   "            <name>name 3</name>\n" +
-   "            <address>address 3</address>\n" +
-   "         </compositeKey>\n" +
-   "         <mapEntry>\n" +  
-   "            <name>c</name>\n"+
-   "            <value>example 3</value>\n"+
-   "         </mapEntry>" + 
-   "      </entry>" +
-   "      <entry>" +
-   "         <compositeKey>\n" +
-   "            <name>name 4</name>\n" +
-   "            <address>address 4</address>\n" +
-   "         </compositeKey>\n" +
-   "         <mapEntry>\n" +  
-   "            <name>d</name>\n"+
-   "            <value>example 4</value>\n"+
-   "         </mapEntry>" + 
-   "      </entry>" +
-   "   </map>\n"+
-   "</complexMap>"; 
-   
-   private static final String PRIMITIVE_MAP =      
-   "<primitiveMap>\n"+
-   "   <table>\n"+   
-   "      <entry>\n" +
-   "         <string>one</string>\n" +
-   "         <bigDecimal>1.0</bigDecimal>\n" +
-   "      </entry>\n"+
-   "      <entry>" +
-   "         <string>two</string>\n" +
-   "         <bigDecimal>2.0</bigDecimal>\n" +
-   "      </entry>\n"+
-   "      <entry>" +
-   "         <string>three</string>\n" +
-   "         <bigDecimal>3.0</bigDecimal>\n" +
-   "      </entry>\n"+
-   "      <entry>" +
-   "         <string>four</string>\n" +
-   "         <bigDecimal>4.0</bigDecimal>\n" +
-   "      </entry>\n"+
-   "   </table>\n"+
-   "</primitiveMap>";  
-   
-   private static final String PRIMITIVE_VALUE_OVERRIDE_MAP =      
-   "<primitiveValueOverrideMap>\n"+
-   "   <map>\n"+   
-   "      <entry>\n" +
-   "         <string>one</string>\n" +
-   "         <decimal>1.0</decimal>\n" +
-   "      </entry>\n"+
-   "      <entry>" +
-   "         <string>two</string>\n" +
-   "         <decimal>2.0</decimal>\n" +
-   "      </entry>\n"+
-   "      <entry>" +
-   "         <string>three</string>\n" +
-   "         <decimal>3.0</decimal>\n" +
-   "      </entry>\n"+
-   "      <entry>" +
-   "         <string>four</string>\n" +
-   "         <decimal>4.0</decimal>\n" +
-   "      </entry>\n"+
-   "   </map>\n"+
-   "</primitiveValueOverrideMap>";
-   
-   private static final String PRIMITIVE_VALUE_KEY_OVERRIDE_MAP =      
-   "<primitiveValueKeyOverrideMap>\n"+
-   "   <map>\n"+   
-   "      <item>\n" +
-   "         <text>one</text>\n" +
-   "         <decimal>1.0</decimal>\n" +
-   "      </item>\n"+
-   "      <item>" +
-   "         <text>two</text>\n" +
-   "         <decimal>2.0</decimal>\n" +
-   "      </item>\n"+
-   "      <item>" +
-   "         <text>three</text>\n" +
-   "         <decimal>3.0</decimal>\n" +
-   "      </item>\n"+
-   "      <item>" +
-   "         <text>four</text>\n" +
-   "         <decimal>4.0</decimal>\n" +
-   "      </item>\n"+
-   "   </map>\n"+
-   "</primitiveValueKeyOverrideMap>"; 
-   
-   private static final String PRIMITIVE_INLINE_MAP =            
-   "<primitiveInlineMap>\n"+ 
-   "   <entity>\n" +
-   "      <string>one</string>\n" +
-   "      <bigDecimal>1.0</bigDecimal>\n" +
-   "   </entity>\n"+
-   "   <entity>" +
-   "      <string>two</string>\n" +
-   "      <bigDecimal>2.0</bigDecimal>\n" +
-   "   </entity>\n"+
-   "   <entity>" +
-   "      <string>three</string>\n" +
-   "      <bigDecimal>3.0</bigDecimal>\n" +
-   "   </entity>\n"+
-   "   <entity>" +
-   "      <string>four</string>\n" +
-   "      <bigDecimal>4.0</bigDecimal>\n" +
-   "   </entity>\n"+
-   "</primitiveInlineMap>";   
-   
-   
-   @Root
-   private static class EntryMap {      
-      
-      @ElementMap(key="key", attribute=true)
-      private Map<String, MapEntry> map;
-      
-      public String getValue(String name) {
-         return map.get(name).value;
-      }
-   }
+   private static final String EMPTY_AS_NULL = 
+   "<complexMap>\r\n" +
+   "   <map class='java.util.HashMap'>\r\n" +
+   "      <entry>\r\n" +
+   "         <mapEntry>\r\n" +
+   "            <name>3</name>\r\n" +
+   "            <value>3</value>\r\n" +
+   "         </mapEntry>\r\n" +
+   "      </entry>\r\n" +
+   "      <entry>\r\n" +
+   "         <compositeKey>\r\n" +
+   "            <name>name.1</name>\r\n" +
+   "            <address>address.1</address>\r\n" +
+   "         </compositeKey>\r\n" +
+   "         <mapEntry>\r\n" +
+   "            <name>1</name>\r\n" +
+   "            <value>1</value>\r\n" +
+   "         </mapEntry>\r\n" +
+   "      </entry>\r\n" +
+   "      <entry>\r\n" +
+   "         <compositeKey>\r\n" +
+   "            <name>name.2</name>\r\n" +
+   "            <address>address.2</address>\r\n" +
+   "         </compositeKey>\r\n" +
+   "         <mapEntry>\r\n" +
+   "            <name>2</name>\r\n" +
+   "            <value>2</value>\r\n" +
+   "         </mapEntry>\r\n" +
+   "      </entry>\r\n" +
+   "      <entry>\r\n" +
+   "         <compositeKey>\r\n" +
+   "            <name>name.4</name>\r\n" +
+   "            <address>address.4</address>\r\n" +
+   "         </compositeKey>\r\n" +
+   "      </entry>\r\n" +
+   "   </map>\r\n" +
+   "</complexMap>\r\n";
    
    @Root
    private static class MapEntry {
@@ -214,16 +69,16 @@ public class MapNullTest extends ValidationTestCase {
          this.name = name;
          this.value = value;
       }
-   }
-   
-   @Root
-   private static class StringMap {
       
-      @ElementMap(key="letter", attribute=true, data=true)
-      private Map<String, String> map;
-      
-      public String getValue(String name) {
-         return map.get(name);
+      public boolean equals(Object other) {
+         if(other instanceof MapEntry) {
+            MapEntry entry = (MapEntry) other;
+            
+            if(entry.value.equals(value)) {
+               return entry.name.equals(name);
+            }
+         }
+         return false;         
       }
    }
    
@@ -238,7 +93,12 @@ public class MapNullTest extends ValidationTestCase {
       }
       
       public String getValue(CompositeKey key) {
-         return map.get(key).value;
+         MapEntry entry = map.get(key);
+         
+         if(entry != null) {
+            return entry.value;
+         }
+         return null;
       }
    }
    
@@ -260,7 +120,7 @@ public class MapNullTest extends ValidationTestCase {
          this.address = address;
       }
       
-      public int hashCode() {
+      public int hashCode() {         
          return name.hashCode() + address.hashCode();
       }
       
@@ -318,5 +178,14 @@ public class MapNullTest extends ValidationTestCase {
       complexMap.map.put(new CompositeKey("name.4", "address.4"), null);
       
       validate(complexMap, serializer);
+      
+      ComplexMap emptyNull = serializer.read(ComplexMap.class, EMPTY_AS_NULL);
+      
+      assertEquals(emptyNull.getValue(new CompositeKey("name.1", "address.1")), "1");
+      assertEquals(emptyNull.getValue(new CompositeKey("name.2", "address.2")), "2");
+      assertEquals(emptyNull.getValue(null), "3");
+      assertEquals(emptyNull.getValue(new CompositeKey("name.4", "address.4")), null);
+      
+      validate(emptyNull, serializer);
    }
 }
