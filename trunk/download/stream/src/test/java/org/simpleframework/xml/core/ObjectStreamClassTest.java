d7319670a46559a49d5bfb5b70232255aaf2392d
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2009-01-26 20:04:21 +0000

    Update the instantiation

--- trunk/download/stream/src/test/java/org/simpleframework/xml/core/ObjectStreamClassTest.java
+++ trunk/download/stream/src/test/java/org/simpleframework/xml/core/ObjectStreamClassTest.java
@@ -6,9 +6,12 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectStreamClass;
 import java.io.Serializable;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
 import junit.framework.TestCase;
+import sun.reflect.ReflectionFactory;
 
 public class ObjectStreamClassTest extends TestCase {
    
@@ -16,6 +19,14 @@ public class ObjectStreamClassTest extends TestCase {
       private String name;
       private String value;
       public Tuple(String name, String value) {
+         this.name = name;
+         this.value = value;
+      }
+   }
+   public static class Example {
+      private String name;
+      private String value;
+      public Example(String name, String value) {
          this.name = name;
          this.value = value;
       }
@@ -45,5 +56,61 @@ public class ObjectStreamClassTest extends TestCase {
       assertEquals(tuple.name, null);
       assertEquals(tuple.value, null);
    }
+   
+   public void testInstantiationWithSerialization() throws Exception {
+      Constructor constructor = getSerializableConstructor(Example.class);
+      
+      if(!constructor.isAccessible()) {
+         constructor.setAccessible(true);
+      }
+      Example example = (Example)constructor.newInstance(null);
+      
+      assertEquals(example.name, null);
+      assertEquals(example.value, null);
+   }
+   
+   private static Constructor getSerializableConstructor(Class cl) throws Exception {
+      Constructor constructors = ReflectionFactory.class.getDeclaredConstructor();
+      
+      if(!constructors.isAccessible()) {
+         constructors.setAccessible(true);
+      }
+      ReflectionFactory reflFactory = (ReflectionFactory)constructors.newInstance();
+      Class initCl = Object.class;
+
+      try {
+          Constructor cons = initCl.getDeclaredConstructor(new Class[0]);
+          int mods = cons.getModifiers();
+          if ((mods & Modifier.PRIVATE) != 0 ||
+         ((mods & (Modifier.PUBLIC | Modifier.PROTECTED)) == 0 &&
+          !packageEquals(cl, initCl)))
+          {
+         return null;
+          }
+          cons = reflFactory.newConstructorForSerialization(cl, cons);
+          cons.setAccessible(true);
+          return cons;
+      } catch (NoSuchMethodException ex) {
+          return null;
+      }
+   }
+   
+   private static boolean packageEquals(Class cl1, Class cl2) {
+      return (cl1.getClassLoader() == cl2.getClassLoader() &&
+         getPackageName(cl1).equals(getPackageName(cl2)));
+       }
+
+       /**
+        * Returns package name of given class.
+        */
+       private static String getPackageName(Class cl) {
+      String s = cl.getName();
+      int i = s.lastIndexOf('[');
+      if (i >= 0) {
+          s = s.substring(i + 2);
+      }
+      i = s.lastIndexOf('.');
+      return (i >= 0) ? s.substring(0, i) : "";
+       }
 
 }
