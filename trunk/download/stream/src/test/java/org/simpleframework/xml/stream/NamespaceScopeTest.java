c2b9421243bdf66f692d840ef765889adaaa76c6
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2009-02-20 13:07:28 +0000

    Remove scrap

--- trunk/download/stream/src/test/java/org/simpleframework/xml/stream/NamespaceScopeTest.java
+++ trunk/download/stream/src/test/java/org/simpleframework/xml/stream/NamespaceScopeTest.java
@@ -8,22 +8,22 @@ public class NamespaceScopeTest extends ValidationTestCase {
    
    private static final String EMPTY_OVERRIDE =
    "<root xmlns='http://www.default.com/'>\n"+ // http://www.default.com/
-   "  <entry xmlns=''>\n"+ 
-   "    <p:book xmlns:p='http://www.example.com/book'>\n"+ // http://www.example.com/book
-   "      <author>saurabh</author>\n"+ // empty 
-   "      <p:title>simple xml</p:title>\n"+ // http://www.example.com/book
-   "      <p:isbn>ISB-16728-10</p:isbn>\n"+ // http://www.example.com/book
-   "    </p:book>\n"+
-   "  </entry>\n"+
+   "<entry xmlns=''>\n"+ 
+   "<p:book xmlns:p='http://www.example.com/book'>\n"+ // http://www.example.com/book
+   "<author>saurabh</author>\n"+ // empty 
+   "<p:title>simple xml</p:title>\n"+ // http://www.example.com/book
+   "<p:isbn>ISB-16728-10</p:isbn>\n"+ // http://www.example.com/book
+   "</p:book>\n"+
+   "</entry>\n"+
    "</root>";    
    
    private static final String DEFAULT_FIRST = 
    "<root xmlns='http://www.default.com/'>\n"+ // http://www.default.com/
-   "  <p:book xmlns:p='http://www.example.com/book'>\n"+ // http://www.example.com/book
-   "    <author>saurabh</author>\n"+ // http://www.default.com/
-   "    <title>simple xml</title>\n"+ // http://www.default.com/
-   "    <isbn>ISB-16728-10</isbn>\n"+ // http://www.default.com/
-   "  </p:book>\n"+
+   "<p:book xmlns:p='http://www.example.com/book'>\n"+ // http://www.example.com/book
+   "<author>saurabh</author>\n"+ // http://www.default.com/
+   "<title>simple xml</title>\n"+ // http://www.default.com/
+   "<isbn>ISB-16728-10</isbn>\n"+ // http://www.default.com/
+   "</p:book>\n"+
    "</root>";    
    
    public void testEmptyOverride() throws Exception {
