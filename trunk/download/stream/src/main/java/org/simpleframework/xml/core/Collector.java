970305ef4f648ff4cd6f13422e4e27efa7c49af6
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2009-07-08 11:07:05 +0000

    Update the documentation

--- trunk/download/stream/src/main/java/org/simpleframework/xml/core/Collector.java
+++ trunk/download/stream/src/main/java/org/simpleframework/xml/core/Collector.java
@@ -25,8 +25,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 /**
- * The <code>Collector</code> object is used to store pointers for
- * a deserialized object. Each pointer contains the label and value
+ * The <code>Collector</code> object is used to store variables for
+ * a deserialized object. Each variable contains the label and value
  * for a field or method. The <code>Composite</code> object uses
  * this to store deserialized values before committing them to the
  * objects methods and fields. 
@@ -37,6 +37,9 @@ import java.util.Iterator;
  */
 class Collector implements Criteria {
    
+   /**
+    * This is the registry containing all the variables collected.
+    */
    private final Registry registry;
    
    /**
@@ -46,31 +49,60 @@ class Collector implements Criteria {
    
    /**
     * Constructor for the <code>Collector</code> object. This is 
-    * used to store pointers to an objects fields and methods.
-    * Each pointer is stored using the name of the label.
+    * used to store variables for an objects fields and methods.
+    * Each variable is stored using the name of the label.
+    * 
+    * @param context this is the context for the deserialization
     */
    public Collector(Context context) {
       this.registry = new Registry();
       this.context = context;
    }
    
+   /**
+    * This is used to get the <code>Variable</code> that represents
+    * a deserialized object. The variable contains all the meta
+    * data for the field or method and the value that is to be set
+    * on the method or field.
+    * 
+    * @param name this is the name of the variable to be acquired
+    * 
+    * @return this returns the named variable if it exists
+    */
    public Variable get(String name) {
       return registry.get(name);
-   }  
-
+   } 
+   
+   /**
+    * This is used to remove the <code>Variable</code> from this
+    * criteria object. When removed, the variable will no longer be
+    * used to set the method or field when the <code>commit</code>
+    * method is invoked.
+    * 
+    * @param name this is the name of the variable to be removed
+    * 
+    * @return this returns the named variable if it exists
+    */
    public Variable remove(String name) {
       return registry.remove(name);
    }
-
+   
+   /**
+    * This is used to acquire an iterator over the named variables.
+    * Providing an <code>Iterator</code> allows the criteria to be
+    * used in a for each loop. This is primarily for convenience.
+    * 
+    * @return this returns an iterator of all the variable names
+    */
    public Iterator<String> iterator() {
       return registry.iterator();
    }
    
    /**
-    * This is used to create a <code>Pointer</code> and store it in
-    * the collector. The pointer can be retrieved at a later stage
+    * This is used to create a <code>Variable</code> and set it for
+    * this criteria. The variable can be retrieved at a later stage
     * using the name of the label. This allows for repeat reads as
-    * the pointer can be used to acquire the labels converter.
+    * the variable can be used to acquire the labels converter.
     * 
     * @param label this is the label used to create the pointer
     * @param value this is the value of the object to be read
@@ -101,11 +133,25 @@ class Collector implements Criteria {
 
          contact.set(source, value);
       }
-   }   
+   }  
    
-   
+   /**
+    * The <code>Registry</code> object is used to store variables 
+    * for the collector. All variables are stored under its name so
+    * that they can be later retrieved and used to populate the
+    * object when deserialization of all variables has finished.
+    * 
+    * @author Niall Gallagher
+    */
    private class Registry extends HashMap<String, Variable> {
       
+      /**
+       * This is used to iterate over the names of the variables
+       * in the registry. This is primarily used for convenience
+       * so that the variables can be acquired in a for each loop.
+       * 
+       * @return an iterator containing the names of the variables
+       */
       public Iterator<String> iterator() {
          return keySet().iterator();
       }
