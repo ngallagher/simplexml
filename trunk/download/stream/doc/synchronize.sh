27cc7b7c143969a7f32da646c6d421651b71f9c5
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-02-21 01:15:45 +0000

    Add multi-threaded tests

--- trunk/download/stream/doc/synchronize.sh
+++ trunk/download/stream/doc/synchronize.sh
@@ -1,1 +1,1 @@
-scp -r -v javadoc/* niallg,simple@web.sourceforge.net:/home/groups/s/si/simple/htdocs/download/stream/doc/javadoc/.
+scp -r -v tutorial/* niallg,simple@web.sourceforge.net:/home/groups/s/si/simple/htdocs/download/stream/doc/tutorial/.
