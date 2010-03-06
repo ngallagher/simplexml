068074576f58fa12c8b69659f5f78fc2cd673063
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-03-06 02:45:29 +0000

    Add helper and location

---
+++ trunk/download/stream/plugin/com.rbsfm.plugin.build/src/com/rbsfm/plugin/build/repository/subversion/ChangeLog.java
@@ -0,0 +1,21 @@
+package com.rbsfm.plugin.build.repository.subversion;
+
+import java.util.Date;
+import java.util.LinkedList;
+
+import org.tmatesoft.svn.core.ISVNLogEntryHandler;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
+
+import com.rbsfm.plugin.build.repository.Change;
+
+class ChangeLog extends LinkedList<Change> implements ISVNLogEntryHandler {
+
+   public void handleLogEntry(SVNLogEntry entry) throws SVNException {
+      String message = entry.getMessage();
+      String author = entry.getAuthor();
+      Date date = entry.getDate();
+      long revision = entry.getRevision();
+      addFirst(new Change(author, message, revision, date));
+   }
+}
