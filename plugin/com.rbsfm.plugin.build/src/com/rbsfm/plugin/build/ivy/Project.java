aee1526c628f407f28c4e4771b710309832476c4
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-03-09 22:26:44 +0000

    Add project parser

---
+++ plugin/com.rbsfm.plugin.build/src/com/rbsfm/plugin/build/ivy/Project.java
@@ -0,0 +1,15 @@
+package com.rbsfm.plugin.build.ivy;
+import org.simpleframework.xml.Attribute;
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+@Root
+public class Project{
+   private @Element Info info;
+   @Root
+   private static class Info{
+      private @Attribute String project;
+   }
+   public String getProject(){
+      return info.project;
+   }
+}
