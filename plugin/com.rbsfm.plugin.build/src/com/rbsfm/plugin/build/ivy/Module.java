fea85a95ccd7c1c3d85270c5c6758fe565c9a203
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-03-11 12:39:20 +0000

    Add dependencies

--- plugin/com.rbsfm.plugin.build/src/com/rbsfm/plugin/build/ivy/Module.java
+++ plugin/com.rbsfm.plugin.build/src/com/rbsfm/plugin/build/ivy/Module.java
@@ -1,22 +1,31 @@
 package com.rbsfm.plugin.build.ivy;
+import java.util.List;
+
 import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
 @Root
 public class Module{
-   private @Element Info info;
+   @Element
+   private Info info;
+   @ElementList(required=false) 
+   private List<Dependency> dependencies;
    @Root
    private static class Info{
       private @Attribute String revision;
       private @Attribute String module;
       private @Attribute String organisation;
    }
-   public String getOrganisation(){
-      return info.organisation;
-   }
    public String getModule(){
       return info.module;
    }
+   public String getOrganisation(){
+      return info.organisation;
+   } 
+   public List<Dependency> getDependencies(){
+      return dependencies;
+   }  
    public String getRevision(){
       String[] split = info.revision.split("-");
       if(split.length > 0){
