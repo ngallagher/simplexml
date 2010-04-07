0eef5754fd151219e912b28903aef4e86776b444
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-04-07 13:24:12 +0000

    Fix the matcher example

--- trunk/download/stream/src/test/java/org/simpleframework/xml/core/MatcherTest.java
+++ trunk/download/stream/src/test/java/org/simpleframework/xml/core/MatcherTest.java
@@ -1,5 +1,7 @@
 package org.simpleframework.xml.core;
 
+import java.io.StringWriter;
+
 import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Namespace;
@@ -31,7 +33,26 @@ public class MatcherTest extends ValidationTestCase {
       }
    }
    
-   private static class ExampleMatcher implements Matcher, Transform<Integer> {
+   @Root
+   private static class EmptyStringExample {
+      
+      @Element(required=false)
+      private String emptyValue;
+      
+      @Element(required=false)
+      private String nullValue;
+      
+      public EmptyStringExample() {
+         super();
+      }
+      
+      public EmptyStringExample(String emptyValue, String nullValue) {
+         this.emptyValue = emptyValue;
+         this.nullValue = nullValue;
+      }
+   }
+   
+   private static class ExampleIntegerMatcher implements Matcher, Transform<Integer> {
 
       public Transform match(Class type) throws Exception {
          if(type == Integer.class) {
@@ -49,14 +70,63 @@ public class MatcherTest extends ValidationTestCase {
       }
    }
    
+   private static class ExampleStringMatcher implements Matcher, Transform<String> {
+
+      public Transform match(Class type) throws Exception {
+         if(type == String.class) {
+            return this;
+         }
+         return null;
+      }
+
+      public String read(String value) throws Exception {         
+         if(value != null) {
+            if(value.equals("[[NULL]]")) {
+               return null;
+            }
+            if(value.equals("[[EMPTY]]")) {
+               return "";
+            }
+         }
+         return value;
+      }
+
+      public String write(String value) throws Exception {
+         if(value == null) {
+            return "[[NULL]]";
+         }
+         if(value.equals("")) {
+            return "[[EMPTY]]";
+         }
+         return value;
+      }
+   }
+   
    public void testMatcher() throws Exception {
-      Matcher matcher = new ExampleMatcher();
+      Matcher matcher = new ExampleIntegerMatcher();
       Serializer serializer = new Persister(matcher);
       Example example = new Example(1, 9999);
       
       serializer.write(example, System.out);
     
       validate(serializer, example);
+   }
+   
+   public void testStringMatcher() throws Exception {
+      Matcher matcher = new ExampleStringMatcher();
+      Serializer serializer = new Persister(matcher);
+      EmptyStringExample original = new EmptyStringExample("", null);
+      StringWriter writer = new StringWriter();
+      
+      serializer.write(original, writer);
+      
+      String text = writer.toString();
+      System.out.println(text);
+      
+      EmptyStringExample recovered = serializer.read(EmptyStringExample.class, text);
+      
+      assertEquals(recovered.emptyValue, original.emptyValue);
+      assertEquals(recovered.nullValue, original.nullValue);
    }
 
 }
