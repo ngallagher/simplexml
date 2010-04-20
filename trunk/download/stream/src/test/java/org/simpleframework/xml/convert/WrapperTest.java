e2fcf19ef7bb1d4f4f159d579b0b0c378b9b74d1
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-04-20 22:32:12 +0000

    Make the @Default elements optional

--- trunk/download/stream/src/test/java/org/simpleframework/xml/convert/WrapperTest.java
+++ trunk/download/stream/src/test/java/org/simpleframework/xml/convert/WrapperTest.java
@@ -54,7 +54,7 @@ public class WrapperTest extends TestCase {
    private static class Entry {
       private String name;
       private String value;
-      public Entry(@Element(name="name") String name, @Element(name="value") String value){
+      public Entry(@Element(name="name", required=false) String name, @Element(name="value", required=false) String value){
          this.name = name;
          this.value = value;
       }
@@ -65,7 +65,7 @@ public class WrapperTest extends TestCase {
    private static class WrapperExample {
       @Convert(WrapperConverter.class)
       private Wrapper wrapper;
-      public WrapperExample(@Element(name="wrapper") Wrapper wrapper) {
+      public WrapperExample(@Element(name="wrapper", required=false) Wrapper wrapper) {
          this.wrapper = wrapper;
       }
    }
