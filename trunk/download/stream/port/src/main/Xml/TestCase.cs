92df411d2e8a5bc90ec2272568080ff1aec96032
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-04-25 21:55:59 +0000

    Remove the old classes

---
+++ trunk/download/stream/port/src/main/Xml/TestCase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace SimpleFramework.Xml {
+   public class TestCase {
+      public void AssertEquals(Object a, Object b) {
+         if(a != b) {
+            if((a == null && b != null) || (a != null && b == null)) {
+               throw new SystemException("Values are not equal as on is null");
+            }
+            if (a.GetType() != b.GetType()) {
+               throw new SystemException("Comparing objects of different types");
+            }
+            if(!a.Equals(b)) {
+               throw new SystemException("Values are not equal");
+            }
+         }
+      }
+      public void AssertNotNull(Object o) {
+         if(o == null) {
+            throw new SystemException("Value is null");
+         }
+      }
+      public void AssertNull(Object o) {
+         if(o != null) {
+            throw new SystemException("Value is not null");
+         }
+      }
+      public static void Main(String[] list) {
+         Type type = Type.GetType(list[0]);
+         Object value = Activator.CreateInstance(type);
+         foreach (MemberInfo info in type.GetMethods()) {
+            if (info.Name.StartsWith("Test") || info.Name.StartsWith("test")) {
+               type.InvokeMember(info.Name, 
+                  BindingFlags.Public | 
+                  BindingFlags.DeclaredOnly |
+                  BindingFlags.Instance, 
+                  null,
+                  value,
+                  null);
+            }
+         }
+
+      }
+   }
+}
