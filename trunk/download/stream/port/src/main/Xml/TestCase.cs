e9e3bf71504cba654275f39634ed29350983b4a3
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-04-26 20:04:54 +0000

    Update files

--- trunk/download/stream/port/src/main/Xml/TestCase.cs
+++ trunk/download/stream/port/src/main/Xml/TestCase.cs
@@ -3,47 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+using System.Diagnostics;
 
 namespace SimpleFramework.Xml {
    public class TestCase {
       public void AssertEquals(Object a, Object b) {
-         if(a != b) {
-            if((a == null && b != null) || (a != null && b == null)) {
+         if (a != b) {
+            if ((a == null && b != null) || (a != null && b == null)) {
                throw new SystemException("Values are not equal as on is null");
             }
             if (a.GetType() != b.GetType()) {
                throw new SystemException("Comparing objects of different types");
             }
-            if(!a.Equals(b)) {
+            if (!a.Equals(b)) {
                throw new SystemException("Values are not equal");
             }
          }
       }
       public void AssertNotNull(Object o) {
-         if(o == null) {
+         if (o == null) {
             throw new SystemException("Value is null");
          }
       }
       public void AssertNull(Object o) {
-         if(o != null) {
+         if (o != null) {
             throw new SystemException("Value is not null");
          }
       }
-      public static void Main(String[] list) {
-         Type type = Type.GetType(list[0]);
-         Object value = Activator.CreateInstance(type);
+      protected internal void Run() {
+         Type type = GetType();
          foreach (MemberInfo info in type.GetMethods()) {
             if (info.Name.StartsWith("Test") || info.Name.StartsWith("test")) {
-               type.InvokeMember(info.Name, 
-                  BindingFlags.Public | 
+               type.InvokeMember(info.Name,
+                  BindingFlags.Public |
                   BindingFlags.DeclaredOnly |
-                  BindingFlags.Instance, 
+                  BindingFlags.Instance |
+                  BindingFlags.InvokeMethod,
                   null,
-                  value,
+                  this,
                   null);
             }
          }
-
       }
    }
 }
---
+++ trunk/download/stream/port/src/main/Xml/TestSuite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimpleFramework.Xml.Util;
+
+namespace SimpleFramework.Xml {
+   public class TestSuite {
+      public Type[] Suite() {
+         return new Type[] {
+            typeof(ResolverTest)
+         };
+      }
+   }
+}
