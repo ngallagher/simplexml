cd8c7c39158cf2e009fc733736199d8dc234edc7
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-05-01 11:21:56 +0000

    Update the source code

---
+++ collections/HashCacheTest.cs
@@ -0,0 +1,24 @@
+﻿#region Using directives
+using SimpleFramework.Xml;
+using System.Collections.Generic;
+using System;
+#endregion
+namespace SimpleFramework.Xml {
+   public class HashCacheTest : TestCase {
+      private const int LOAD_COUNT = 100000;
+      public void TestCache() {
+         Cache<String, String> cache = new HashCache<String, String>();
+         Map<String, String> map = new HashMap<String, String>();
+         for(int i = 0; i < LOAD_COUNT; i++) {
+            String key = i.ToString();
+            cache.Insert(key, key);
+            map.Put(key, key);
+         }
+         for(int i = 0; i < LOAD_COUNT; i++) {
+            String key = i.ToString();
+            AssertEquals(cache.Fetch(key), key);
+            AssertEquals(map.Get(key), cache.Fetch(key));
+         }
+      }
+   }
+}
---
+++ collections/Match.cs
@@ -0,0 +1,52 @@
+﻿#region License
+//
+// Match.cs March 2002
+//
+// Copyright (C) 2001, Niall Gallagher <niallg@users.sf.net>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+#endregion
+
+#region Using directives
+using System;
+#endregion
+
+namespace SimpleFramework.Xml {
+
+   /// <summary>
+   /// This object is stored within a <c>Resolver</c> so that it
+   /// can be retrieved using a string that matches its pattern. Any
+   /// object that : this can be inserted into the resolver and
+   /// retrieved using a string that matches its pattern. For example
+   /// take the following pattern "*.html" this will match the string
+   /// "/index.html" or "readme.html". This object should be extended
+   /// to add more XML attributes and elements, which can be retrieved
+   /// when the <c>Match</c> object is retrieve from a resolver.
+   /// </summary>
+   public abstract class Match {
+
+      /// <summary>
+      /// This is the pattern string that is used by the resolver. A
+      /// pattern can consist of a "*" character and a "?" character
+      /// to match the pattern. Implementations of this class should
+      /// provide the pattern so that it can be used for resolution.
+      /// </summary>
+      /// <returns>
+      /// this returns the pattern that is to be matched
+      /// </returns>
+      public abstract String Pattern {
+         get;
+      }
+   }
+}
