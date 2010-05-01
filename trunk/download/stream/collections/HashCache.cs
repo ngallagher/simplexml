3c7c4e866bdae6fa88cfafbf5df2bed4b1184c54
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2010-05-01 10:19:27 +0000

    Add cache implementations

---
+++ trunk/download/stream/collections/HashCache.cs
@@ -0,0 +1,304 @@
+﻿#region License
+//
+// HashCache.cs July 2006
+//
+// Copyright (C) 2006, Niall Gallagher <niallg@users.sf.net>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
+#endregion
+
+#region Using directives
+using System.Collections.Generic;
+using System;
+#endregion
+
+namespace SimpleFramework.Xml.Util {
+
+   /// <summary>
+   /// The <c>HashCache</c> object is an implementation of a cache
+   /// that holds on to cached items only if the key remains in memory.
+   /// This is effectively like a concurrent hash map with weak keys, it
+   /// ensures that multiple threads can concurrently access weak hash
+   /// maps in a way that lowers contention for the locks used.
+   /// </summary>
+   public class HashCache<K, V> : Cache<K, V> {
+
+      /// <summary>
+      /// This is used to store a list of segments for the cache.
+      /// </summary>
+      private SegmentList list;
+
+      /// <summary>
+      /// Constructor for the <c>HashCache</c> object. This is
+      /// used to create a cache that stores values in such a way that
+      /// when the key is garbage collected the value is removed from
+      /// the map. This is similar to the concurrent hash map.
+      /// </summary>
+      public HashCache() : this (10) {
+      }
+
+      /// <summary>
+      /// Constructor for the <c>HashCache</c> object. This is
+      /// used to create a cache that stores values in such a way that
+      /// when the key is garbage collected the value is removed from
+      /// the map. This is similar to the concurrent hash map.
+      /// </summary>
+      /// <param name="size">
+      /// This is the number of segments within the cache.
+      /// </param>
+      public HashCache(int size) {
+         this.list = new SegmentList(size);
+      }
+
+      /// <summary>
+      /// This method is used to insert a key value mapping in to the
+      /// cache. The value can later be retrieved or removed from the
+      /// cache if desired. If the value associated with the key is
+      /// null then nothing is stored within the cache.
+      /// </summary>
+      /// <param name="key">
+      /// This is the key to cache the provided value to.
+      /// </param>
+      /// <param name="value">
+      /// This is the value that is to be cached.
+      /// </param>
+      public void Cache(K key, V value) {
+         Map(key).Cache(key, value);
+      }
+
+      /// <summary>
+      /// This is used to exclusively take the value mapped to the
+      /// specified key from the cache. Invoking this is effectively
+      /// removing the value from the cache.
+      /// </summary>
+      /// <param name="key">
+      /// This is the key to acquire the cache value with.
+      /// </param>
+      /// <returns>
+      /// This returns the value mapped to the specified key.
+      /// </returns>
+      public V Take(K key) {
+         return Map(key).Take(key);
+      }
+
+      /// <summary>
+      /// This method is used to get the value from the cache that is
+      /// mapped to the specified key. If there is no value mapped to
+      /// the specified key then this method will return a null.
+      /// </summary>
+      /// <param name="key">
+      /// This is the key to acquire the cache value with.
+      /// </param>
+      /// <returns>
+      /// This returns the value mapped to the specified key.
+      /// </returns>
+      public V Fetch(K key) {
+         return Map(key).Fetch(key);
+      }
+
+      /// <summary>
+      /// This is used to determine whether the specified key exists
+      /// with in the cache. Typically this can be done using the
+      /// fetch method, which will acquire the object.
+      /// </summary>
+      /// <param name="key">
+      /// This is the key to check within this segment.
+      /// </param>
+      /// <returns>
+      /// True if the specified key is within the cache.
+      /// </returns>
+      public bool Contains(K key) {
+         return Map(key).Contains(key);
+      }
+
+      /// <summary>
+      /// This method is used to acquire a <c>Segment</c> using
+      /// the keys has code. This method effectively uses the hash to
+      /// find a specific segment within the fixed list of segments.
+      /// </summary>
+      /// <param name="key">
+      /// This is the key used to acquire the segment.
+      /// </param>
+      /// <returns>
+      /// This returns the segment used to get acquire value.
+      /// </returns>
+      public Segment Map(K key) {
+         return list.Get(key);
+      }
+
+      /// <summary>
+      /// This is used to maintain a list of segments. All segments that
+      /// are stored by this object can be acquired using a given key.
+      /// The keys hash is used to select the segment, this ensures that
+      /// all read and write operations with the same key result in the
+      /// same segment object within this list.
+      /// </summary>
+      private class SegmentList {
+
+         /// <summary>
+         /// The list of segment objects maintained by this object.
+         /// </summary>
+         private List<Segment> list;
+
+         /// <summary>
+         /// Represents the number of segments this object maintains.
+         /// </summary>
+         private int size;
+
+         /// <summary>
+         /// Constructor for the <c>SegmentList</c> object. This
+         /// is used to create a list of weak hash maps that can be
+         /// acquired using the hash code of a given key.
+         /// </summary>
+         /// <param name="size">
+         /// This is the number of hash maps to maintain.
+         /// </param>
+         public SegmentList(int size) {
+            this.list = new List<Segment>();
+            this.size = size;
+            this.Create(size);
+         }
+
+         /// <summary>
+         /// This is used to acquire the segment using the given key.
+         /// The keys hash is used to determine the index within the
+         /// list to acquire the segment, which is a synchronized weak
+         /// hash map storing the key value pairs for a given hash.
+         /// </summary>
+         /// <param name="key">
+         /// This is the key used to determine the segment.
+         /// </param>
+         /// <returns>
+         /// The segment that is stored at the resolved hash.
+         /// </returns>
+         public Segment Get(K key) {
+            int segment = Segment(key);
+
+            if(segment < size) {
+               return list[segment];
+            }
+            return null;
+         }
+
+         /// <summary>
+         /// Upon initialization the segment list is populated in such
+         /// a way that synchronization is not needed. Each segment is
+         /// created and stored in an increasing index within the list.
+         /// </summary>
+         /// <param name="size">
+         /// This is the number of segments to be used.
+         /// </param>
+         public void Create(int size) {
+            int count = size;
+
+            while(count-- > 0) {
+               list.Add(new Segment());
+            }
+         }
+
+         /// <summary>
+         /// This method performs the translation of the key hash code
+         /// to the segment index within the list. Translation is done
+         /// by acquiring the modulus of the hash and the list size.
+         /// </summary>
+         /// <param name="key">
+         /// This is the key used to resolve the index.
+         /// </param>
+         /// <returns>
+         /// The index of the segment within the list.
+         /// </returns>
+         public int Segment(K key) {
+            return Math.Abs(key.GetHashCode() % size);
+         }
+      }
+
+      /// <summary>
+      /// The segment is effectively a synchronized weak hash map. If is
+      /// used to store the key value pairs in such a way that they are
+      /// kept only as long as the garbage collector does not collect
+      /// the key. This ensures the cache does not cause memory issues.
+      /// </summary>
+      private class Segment : HashMap<K, V> {
+
+         /// <summary>
+         /// This method is used to insert a key value mapping in to the
+         /// cache. The value can later be retrieved or removed from the
+         /// cache if desired. If the value associated with the key is
+         /// null then nothing is stored within the cache.
+         /// </summary>
+         /// <param name="key">
+         /// This is the key to cache the provided value to.
+         /// </param>
+         /// <param name="value">
+         /// This is the value that is to be cached.
+         /// </param>
+         public void Cache(K key, V value) {
+            lock(this) {
+               Put(key, value);
+            }
+         }
+
+         /// <summary>
+         /// This method is used to get the value from the cache that is
+         /// mapped to the specified key. If there is no value mapped to
+         /// the specified key then this method will return a null.
+         /// </summary>
+         /// <param name="key">
+         /// This is the key to acquire the cache value with.
+         /// </param>
+         /// <returns>
+         /// This returns the value mapped to the specified key.
+         /// </returns>
+         public V Fetch(K key) {
+            lock(this) {
+               return Get(key);
+            }
+         }
+
+         /// <summary>  
+         /// This is used to exclusively take the value mapped to the
+         /// specified key from the cache. Invoking this is effectively
+         /// removing the value from the cache.
+         /// </summary>
+         /// <param name="key">
+         /// This is the key to acquire the cache value with.
+         /// </param>
+         /// <returns>
+         /// This returns the value mapped to the specified key.
+         /// </returns>
+         public V Take(K key) {
+            lock(this) {
+               return Remove(key);
+            }
+         }
+
+         /// <summary>
+         /// This is used to determine whether the specified key exists
+         /// with in the cache. Typically this can be done using the
+         /// fetch method, which will acquire the object.
+         /// </summary>
+         /// <param name="key">
+         /// This is the key to check within this segment.
+         /// </param>
+         /// <returns>
+         /// True if the specified key is within the cache.
+         /// </returns>
+         public bool Contains(K key) {
+            lock(this) {
+               return Contains(key);
+            }
+         }
+      }
+   }
+}
--- trunk/download/stream/collections/HashMap.cs
+++ trunk/download/stream/collections/HashMap.cs
@@ -17,21 +17,68 @@
 // permissions and limitations under the License.
 //
 #endregion
+
 #region Using directives
 using System;
 using System.Collections.Generic;
 #endregion
+
 namespace SimpleFramework.Xml {
+
+   /// <summary>
+   /// The <c>HashMap</c> class represents a data structure that maintains 
+   /// mappings between key value pairs. Unlike the <c>Dictionary</c> that
+   /// is provided with the standard .NET collections this is not use fail
+   /// fast lookups. If a mapping is requested that does not exist a null
+   /// is returned. This is similar to the Java <c>Map</c> collection but
+   /// is stripped down to the most basic operations.
+   /// <p>
+   /// In addition to the standard Java collection methods this map has an
+   /// indexer that accepts a key, which makes getting and setting mappings
+   /// much easier. A <c>HashMap</c> instance must be qualified with key 
+   /// and value generic parameters. This is backed using the standard .NET
+   /// dictionary and acts as an adapter to facilitate ease of porting.
+   /// </summary>
    public class HashMap<K, V> : Map<K, V> {
+
+      /// <summary>
+      /// This is the dictionary that backs this hash table instance.
+      /// </summary>
       private readonly Dictionary<K, V> table;
+
+      /// <summary>
+      /// Constructor for the <c>HashMap</c> object. This is used to
+      /// create a map using the .NET dictionary implementation. All of
+      /// the operations performed on this map are delegated to the 
+      /// underlying dictionary instance. This is an unsynchronized map.
+      /// </summary>
       public HashMap() {
          this.table = new Dictionary<K, V>();
       }
+
+      /// <summary>
+      /// This is used to determine the number of mappings that have been
+      /// inserted in to the map. If size is zero <c>Empty</c> will be
+      /// true. This can be used to iterate over the key and value arrays.
+      /// </summary>
+      /// <returns>
+      /// The name number of mappings that have been added to the map.
+      /// </returns>
       public override int Size {
          get {
             return table.Count;
          }
       }
+
+      /// <summary>
+      /// This is used to return an array of keys. Depending on the map
+      /// implementation keys may be returned in a random order or in an
+      /// ordered manner. Keys are returned as an array so that can be
+      /// used in a <c>foreach</c> loop. 
+      /// </summary>
+      /// <returns>
+      /// An array of keys representing the keys for each mapping.
+      /// </returns>
       public override K[] Keys {
          get {
             K[] list = new K[Size];
@@ -42,6 +89,15 @@ namespace SimpleFramework.Xml {
             return list;
          }
       }
+
+      /// <summary>
+      /// This is used to return an array of values. Depending on the map
+      /// implementation values may be returned in a random order This is 
+      /// provided for convenient use in a <c>foreach</c> loop.
+      /// </summary>
+      /// <returns>
+      /// An array of values that have been mapped within this instance.
+      /// </returns>
       public override V[] Values {
          get {
             V[] list = new V[Size];
@@ -52,6 +108,19 @@ namespace SimpleFramework.Xml {
             return list;
          }
       }
+
+      /// <summary>
+      /// This is used to acquire the value associated with the specified
+      /// key. If there is no mapping within the <c>Map</c> this returns 
+      /// null. Given that mappings can be made to a null value this can
+      /// not be used to determine if a mapping exists within the instance.
+      /// </summary>
+      /// <param name="key">
+      /// This is the key that is used to acquire the mapped value.
+      /// </param>
+      /// <returns>
+      /// This is used to acquire the value associated with the key.
+      /// </returns>
       public override V Get(K key) {
          bool exists = Contains(key);
 
@@ -60,6 +129,22 @@ namespace SimpleFramework.Xml {
          }
          return default(V);
       }
+
+      /// <summary>
+      /// This is used to establish a mapping within the map. If a mapping
+      /// already exists with the specified key then this will return the
+      /// value that was previously associated with the key. Once mapped
+      /// the <c>Get</c> method or indexer can be used to get the value.
+      /// </summary>
+      /// <param name="key">
+      /// This is the key that is used to establish the mapping.
+      /// </param>
+      /// <param name="value">
+      /// This is the value that will be associated with the given key.
+      /// </param>
+      /// <returns>
+      /// Returns the value previously associated with the key, or null.
+      /// </returns>
       public override V Put(K key, V value) {
          V entry = Get(key);
 
@@ -68,6 +153,19 @@ namespace SimpleFramework.Xml {
          }
          return entry;
       }
+
+      /// <summary>
+      /// This is used to remove a mapping from the <c>Map</c>. This acts
+      /// much like a take method would for a cache, in that it will 
+      /// the value of the mapping that has just been removed. If the
+      /// mapping did not exist then this method will return null.
+      /// </summary>
+      /// <param name="key">
+      /// This is the key to be searched for within this map instance.
+      /// </param>
+      /// <returns>
+      /// This returns the value associated with the mapping if it exists.
+      /// </returns>
       public override V Remove(K key) {
          V value = Get(key);
 
@@ -76,9 +174,28 @@ namespace SimpleFramework.Xml {
          }
          return value;
       }
+
+      /// <summary>
+      /// This is used to determine if a mapping exists within the map. If
+      /// this returns true then the <c>Keys</c> property will return the
+      /// specified key within the array of keys. However, if the key is 
+      /// mapped to null then the <c>Get</c> method will return null.
+      /// </summary>
+      /// <param name="key">
+      /// This is the key to be searched for within this map instance.
+      /// </param>
+      /// <returns>
+      /// This will return true if the mapping exists within the instance.
+      /// </returns>
       public override bool Contains(K key) {
          return table.ContainsKey(key);
       }
+
+      /// <summary>
+      /// This is used to clear the <c>Map</c> instance. Clearing the map
+      /// instance ensures that there are no mappings maintained. Further
+      /// attempts to get via the <c>Get</c> method will return null.
+      /// </summary>
       public override void Clear() {
          table.Clear();
       }
