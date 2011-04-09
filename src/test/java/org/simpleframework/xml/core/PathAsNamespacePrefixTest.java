1648a78c855058e448d6afa1799b4777b4dbfa8f
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2011-04-09 07:17:20 +0000

    Ensure we can bring a namespace prefix in scope

--- src/test/java/org/simpleframework/xml/core/PathAsNamespacePrefixTest.java
+++ src/test/java/org/simpleframework/xml/core/PathAsNamespacePrefixTest.java
@@ -2,6 +2,7 @@ package org.simpleframework.xml.core;
 
 import java.io.StringWriter;
 
+import org.simpleframework.xml.Default;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Namespace;
 import org.simpleframework.xml.Path;
@@ -29,6 +30,34 @@ public class PathAsNamespacePrefixTest extends ValidationTestCase {
          return id;
       }
    }
+   
+   @Root
+   private static class PathWithPrefixOutOfScope {
+      @Path("x/y/p:z")
+      @Namespace(reference="http://www.x.com/y")
+      @Variant({
+         @Element(name="name"),
+         @Element(name="login"),
+         @Element(name="user")
+      })
+      private final String id;
+      public PathWithPrefixOutOfScope(@Element(name="name") String id){
+         this.id = id;
+      }
+      public String getId(){
+         return id;
+      }
+   }
+   
+   @Default
+   @Namespace(prefix="p", reference="http://www.x.com/p")
+   private static class BringPrefixInScope {
+      private final PathWithPrefixOutOfScope example;
+      public BringPrefixInScope(@Element(name="example") PathWithPrefixOutOfScope example){
+         this.example = example;
+      }
+   }
+   
    public void testPathWithPrefix() throws Exception{
       Persister persister = new Persister();
       PathWithPrefix example = new PathWithPrefix("tim");
@@ -39,5 +68,31 @@ public class PathAsNamespacePrefixTest extends ValidationTestCase {
       PathWithPrefix recovered = persister.read(PathWithPrefix.class, text);
       assertEquals(recovered.getId(), "tim");
       validate(persister, example);
+   }
+   
+   public void testPathWithPrefixOufOfScope() throws Exception{
+      Persister persister = new Persister();
+      PathWithPrefixOutOfScope example = new PathWithPrefixOutOfScope("tim");
+      StringWriter writer = new StringWriter();
+      boolean exception = false;
+      try {
+         persister.write(example, writer);
+      }catch(Exception e) {
+         e.printStackTrace();
+         exception = true;
+      }
+      assertTrue("Prefix that is not in scope should be illegal", exception);
+   }
+   
+   public void testBringPrefixInScope() throws Exception{
+      Persister persister = new Persister();
+      PathWithPrefixOutOfScope example = new PathWithPrefixOutOfScope("tim");
+      BringPrefixInScope parent = new BringPrefixInScope(example);
+      StringWriter writer = new StringWriter();
+      persister.write(parent, writer);
+      System.out.println(writer);
+      BringPrefixInScope recovered = persister.read(BringPrefixInScope.class, writer.toString());
+      assertEquals(recovered.example.getId(), "tim");
+      validate(persister, parent);
    }
 }
