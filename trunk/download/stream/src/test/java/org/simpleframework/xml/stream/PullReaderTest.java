d8da73ffb244305bef8bd7448565267fb378c045
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2011-04-13 10:25:04 +0000

    Ensure we add input stream to test

--- trunk/download/stream/src/test/java/org/simpleframework/xml/stream/PullReaderTest.java
+++ trunk/download/stream/src/test/java/org/simpleframework/xml/stream/PullReaderTest.java
@@ -1,5 +1,7 @@
 package org.simpleframework.xml.stream;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayList;
@@ -12,12 +14,39 @@ import org.xmlpull.v1.XmlPullParser;
 public class PullReaderTest extends TestCase {
 
    private static final String SOURCE =
-   "<?xml version=\"1.0\"?>\n"+
+   "<?xml version=\"1.0\" encoding='UTF-8'?>\n"+
    "<x:override id='12' flag='true' xmlns:x='http://www.x.com/x'>\n"+
    "   <x:text>entry text</x:text>  \n\r"+
    "   <name>some name</name> \n"+
    "   <third>added to schema</third>\n"+
    "</x:override>";
+   
+   public void testInputStream() throws Exception {
+      Provider provider = new PullProvider();
+      byte[] array = SOURCE.getBytes("UTF-8");
+      InputStream source = new ByteArrayInputStream(array);
+      EventReader reader = provider.provide(source);
+      EventNode node = reader.peek();
+      
+      assertEquals(node.getName(), "override");
+      assertEquals(node.getLine(), 2);
+      assertEquals(node.getPrefix(), "x");
+      assertEquals(node.getReference(), "http://www.x.com/x");
+      assertTrue(XmlPullParser.class.isAssignableFrom(node.getSource().getClass()));
+      
+      List<Attribute> attributes = new ArrayList<Attribute>();
+      
+      for(Attribute attribute : node) {
+         attributes.add(attribute);
+      }
+      assertEquals(attributes.size(), 2);
+      assertEquals(attributes.get(0).getName(), "id");
+      assertEquals(attributes.get(0).getValue(), "12");
+      assertTrue(XmlPullParser.class.isAssignableFrom(attributes.get(0).getSource().getClass()));
+      assertEquals(attributes.get(1).getName(), "flag");
+      assertEquals(attributes.get(1).getValue(), "true");
+      assertTrue(XmlPullParser.class.isAssignableFrom(attributes.get(1).getSource().getClass()));
+   }
    
    public void testReader() throws Exception {
       Provider provider = new PullProvider();
