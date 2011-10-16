ebd63e2c7423bdf6c41640b35bae46fddd101676
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2011-10-16 02:06:55 +0000

    Update the util package

--- trunk/download/stream/src/package/java/org/simpleframework/xml/util/package-info.java
+++ trunk/download/stream/src/package/java/org/simpleframework/xml/util/package-info.java
@@ -1,1 +1,6 @@
+/**
+ * Provides a set of utility classes that are used within the framework.
+ * The utility classes provided by this package can also be used to
+ * represent several common XML data structures. 
+ */
 package org.simpleframework.xml.util;
