32e4cb38997e12e71653659f9a068269a2338714
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2011-10-16 02:28:14 +0000

    Update package information

--- trunk/download/stream/src/package/java/org/simpleframework/xml/strategy/package-info.java
+++ trunk/download/stream/src/package/java/org/simpleframework/xml/strategy/package-info.java
@@ -1,9 +1,9 @@
 /**
  * Provides a core set of strategies that can be used for serialization.
  * Stragtegies tell the serialization process what to do when it reads
- * or writes an XML element. A strategy should contain the information
- * required. By default the <code>TreeStrategy</code> is used to the
- * serialization process.
+ * or writes an XML element. A strategy should contain all information
+ * required to serialize and deserialize an object instance. By default 
+ * the <code>TreeStrategy</code> is used by the serialization process.
  * 
  * @see org.simpleframework.xml.core
  * @see org.simpleframework.xml.convert
