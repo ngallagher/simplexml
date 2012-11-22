dc69741882f667c6cf27631541a1f167cc41eaba
niallg <niallg@124fc276-1b20-0410-8d16-dc08e6bff5e9>
2012-11-22 09:27:10 +0000

    Fix docs

--- src/main/java/org/simpleframework/xml/core/DetailScanner.java
+++ src/main/java/org/simpleframework/xml/core/DetailScanner.java
@@ -1,3 +1,21 @@
+/*
+ * DetailScanner.java July 2012
+ *
+ * Copyright (C) 2012, Niall Gallagher <niallg@users.sf.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+
 package org.simpleframework.xml.core;
 
 import java.lang.annotation.Annotation;
@@ -15,21 +33,85 @@ import org.simpleframework.xml.NamespaceList;
 import org.simpleframework.xml.Order;
 import org.simpleframework.xml.Root;
 
+/**
+ * The <code>DetailScanner</code> is used to scan a class for methods
+ * and fields as well as annotations. Scanning a type in this way 
+ * ensures that all its details can be extracted and cached in one
+ * place. This greatly improves performance on platforms that do not
+ * cache reflection well, like Android. 
+ * 
+ * @author Niall Gallagher
+ */
 class DetailScanner implements Detail {
    
+   /**
+    * This contains a list of methods that are extracted for this.
+    */
    private List<MethodDetail> methods;
+   
+   /**
+    * This contains a list of fields that are extracted for this.
+    */
    private List<FieldDetail> fields;
+   
+   /**
+    * This represents the namespace list declared on the type.
+    */
    private NamespaceList declaration;
+   
+   /**
+    * This represents the namespace annotation declared on the type.
+    */
    private Namespace namespace;
+   
+   /**
+    * This represents all the annotations declared for the type.
+    */
    private Annotation[] labels;
+   
+   /**
+    * This represents the default access type declared or the type.
+    */
    private DefaultType access;
+   
+   /**
+    * This is the order annotation that is declared for the type.
+    */
    private Order order;
+   
+   /**
+    * This is the root annotation that is declared for the type.
+    */
    private Root root;
+   
+   /**
+    * This is the type that is represented by this instance.
+    */
    private Class type;
+   
+   /**
+    * This represents the name of the type used for XML elements.
+    */
    private String name;
+   
+   /**
+    * This is used to determine if the default type is required.
+    */
    private boolean required;
+   
+   /**
+    * This is used to determine if strict XML parsing is done.
+    */
    private boolean strict;
    
+   /**
+    * Constructor for the <code>DetailScanner</code> object. This is
+    * used to create a detail object from a type. All of the methods
+    * fields and annotations are extracted so that they can be used
+    * many times over without the need to process them again.
+    * 
+    * @param type this is the type to scan for various details
+    */
    public DetailScanner(Class type) throws Exception {
       this.methods = new LinkedList<MethodDetail>();
       this.fields = new LinkedList<FieldDetail>();
@@ -93,6 +175,13 @@ class DetailScanner implements Detail {
       return !type.isMemberClass();       
    }
    
+   /**
+    * This returns the <code>Root</code> annotation for the class.
+    * The root determines the type of deserialization that is to
+    * be performed and also contains the name of the root element. 
+    * 
+    * @return this returns the name of the object being scanned
+    */
    public Root getRoot() {
       return root;
    }
@@ -110,42 +199,117 @@ class DetailScanner implements Detail {
       return name;
    }
    
+   /**
+    * This returns the type represented by this detail. The type is
+    * the class that has been scanned for annotations, methods and
+    * fields. All super types of this are represented in the detail.
+    * 
+    * @return the type that this detail object represents
+    */
    public Class getType() {
       return type;
    }
    
+   /**
+    * This returns the order annotation used to determine the order
+    * of serialization of attributes and elements. The order is a
+    * class level annotation that can be used only once per class
+    * XML schema. If none exists then this will return null.
+    *  of the class processed by this scanner.
+    * 
+    * @return this returns the name of the object being scanned
+    */
    public Order getOrder() {
       return order;
    }
    
+   /**
+    * This returns the <code>Default</code> annotation access type
+    * that has been specified by this. If no default annotation has
+    * been declared on the type then this will return null.
+    * 
+    * @return this returns the default access type for this type
+    */
    public DefaultType getAccess() {
       return access;
    }
    
+   /**
+    * This returns the <code>Namespace</code> annotation that was
+    * declared on the type. If no annotation has been declared on the
+    * type this will return null as not belonging to any.
+    * 
+    * @return this returns the namespace this type belongs to, if any
+    */
    public Namespace getNamespace() {
       return namespace;
    }
    
+   /**
+    * This returns the <code>NamespaceList</code> annotation that was
+    * declared on the type. A list of namespaces are used to simply 
+    * declare the namespaces without specifically making the type
+    * belong to any of the declared namespaces.
+    * 
+    * @return this returns the namespace declarations, if any
+    */
    public NamespaceList getNamespaceList() {
       return declaration;
    }
    
+   /**
+    * This returns a list of the methods that belong to this type. 
+    * The methods here do not include any methods from the super
+    * types and simply provides a means of caching method data.
+    * 
+    * @return returns the list of methods declared for the type
+    */
    public List<MethodDetail> getMethods() {
       return methods;
    }
    
+   /**
+    * This returns a list of the fields that belong to this type. 
+    * The fields here do not include any fields from the super
+    * types and simply provides a means of caching method data.
+    * 
+    * @return returns the list of fields declared for the type
+    */
    public List<FieldDetail> getFields() {
       return fields;
    }
    
+   /**
+    * This returns the annotations that have been declared for this
+    * type. It is preferable to acquire the declared annotations
+    * from this method as they are cached. Older versions of some
+    * runtime environments, particularly Android, are slow at this.
+    * 
+    * @return this returns the annotations associated with this
+    */
    public Annotation[] getAnnotations() {
       return labels;
    }
 
+   /**
+    * This returns the constructors that have been declared for this
+    * type. It is preferable to acquire the declared constructors
+    * from this method as they are cached. Older versions of some
+    * runtime environments, particularly Android, are slow at this.
+    * 
+    * @return this returns the constructors associated with this
+    */
    public Constructor[] getConstructors() {
       return type.getDeclaredConstructors();
    }
    
+   /**
+    * This is used to acquire the super type for the class that is
+    * represented by this detail. If the super type for the class
+    * is <code>Object</code> then this will return null.
+    * 
+    * @return returns the super type for this class or null
+    */
    public Class getSuper() {
       Class base = type.getSuperclass();
       
@@ -155,12 +319,28 @@ class DetailScanner implements Detail {
       return base;
    }
    
+   /**
+    * This method is used to scan the type for all of its annotations
+    * as well as its methods and fields. Everything that is scanned 
+    * is cached within the instance to ensure that it can be reused
+    * when ever an object of this type is to be scanned.
+    * 
+    * @param type this is the type to scan for details
+    */
    private void scan(Class type) throws Exception {
       methods(type);
       fields(type);
       extract(type);
    }
    
+   /**
+    * This method is used to extract the annotations associated with
+    * the type. Annotations extracted include the <code>Root</code> 
+    * annotation and the <code>Namespace</code> annotation as well as
+    * other annotations that are used to describe the type.
+    * 
+    * @param type this is the type to extract the annotations from
+    */
    private void extract(Class type) throws Exception {
       for(Annotation label : labels) {
          if(label instanceof Namespace) {
@@ -181,6 +361,14 @@ class DetailScanner implements Detail {
       }
    }
    
+   /**
+    * This is used to scan the type for its declared methods. Scanning
+    * of the methods in this way allows the detail to prepare a cache
+    * that can be used to acquire the methods and the associated
+    * annotations. This improves performance on some platforms.
+    * 
+    * @param type this is the type to scan for declared annotations
+    */
    private void methods(Class type) throws Exception {
       Method[] list = type.getDeclaredMethods();
       
@@ -190,6 +378,14 @@ class DetailScanner implements Detail {
       }
    }
    
+   /**
+    * This is used to scan the type for its declared fields. Scanning
+    * of the fields in this way allows the detail to prepare a cache
+    * that can be used to acquire the fields and the associated
+    * annotations. This improves performance on some platforms.
+    * 
+    * @param type this is the type to scan for declared annotations
+    */
    private void fields(Class type) throws Exception {
       Field[] list = type.getDeclaredFields();
       
@@ -198,11 +394,10 @@ class DetailScanner implements Detail {
          fields.add(detail);
       }
    }
-   
 
    /**
     * This is used to set the optional <code>Root</code> annotation for
-    * the class. The root can only be set once, so if a supertype also
+    * the class. The root can only be set once, so if a super type also
     * has a root annotation define it must be ignored. 
     *
     * @param label this is the label used to define the root
@@ -242,7 +437,7 @@ class DetailScanner implements Detail {
    
    /**
     * This is used to set the optional <code>Order</code> annotation for
-    * the class. The order can only be set once, so if a supertype also
+    * the class. The order can only be set once, so if a super type also
     * has a order annotation define it must be ignored. 
     * 
     * @param label this is the label used to define the order
@@ -255,7 +450,7 @@ class DetailScanner implements Detail {
    
    /**
     * This is used to set the optional <code>Default</code> annotation for
-    * the class. The default can only be set once, so if a supertype also
+    * the class. The default can only be set once, so if a super type also
     * has a default annotation define it must be ignored. 
     * 
     * @param label this is the label used to define the defaults
@@ -268,6 +463,15 @@ class DetailScanner implements Detail {
          access = value.value();
       }
    }
+   
+   /**
+    * This is use to scan for <code>Namespace</code> annotations on
+    * the class. Once a namespace has been located then it is used to
+    * populate the internal namespace decorator. This can then be used
+    * to decorate any output node that requires it.
+    * 
+    * @param label the XML annotation to scan for the namespace
+    */
    private void namespace(Annotation label) {
       if(label != null) {
          namespace = (Namespace)label;
@@ -288,6 +492,13 @@ class DetailScanner implements Detail {
       }
    }
    
+   /**
+    * This is used to return a string representation of the detail. 
+    * The string returned from this is the same that is returned
+    * from the <code>toString</code> of the type represented. 
+    * 
+    * @return this returns the string representation of the type
+    */
    public String toString() {
       return type.toString();
    }
